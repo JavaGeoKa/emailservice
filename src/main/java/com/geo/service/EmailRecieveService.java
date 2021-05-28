f6dc5bcddabe7afa068a2d116e87bfb364b6de14
GeoIsrael <MDf4nBNjqrufGnxS@protonmail.com>
2021-05-28 11:39:09 +0300

    flags and zip

--- src/main/java/com/geo/service/EmailRecieveService.java
+++ src/main/java/com/geo/service/EmailRecieveService.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.mail.*;
+import javax.mail.internet.InternetAddress;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -29,37 +30,48 @@ public class EmailRecieveService {
 
 
 
-    @Scheduled(fixedDelay = 10000)
+    @Scheduled(fixedDelay = 5000)
     public void sendSimpleMessage() throws MessagingException, IOException {
         Folder inbox = store.getFolder("INBOX");
         inbox.open(Folder.READ_WRITE);
         Message[] messages = inbox.getMessages();
 
         Arrays.stream(inbox.getMessages()).forEach(m -> {
-
             try {
-                if(!m.getFlags().contains(Flags.Flag.DELETED)) {
-
+                if (!m.getFlags().contains(Flags.Flag.DELETED)) {
                     try {
                         Object content = m.getContent();
                         if (content instanceof String) {
-                            //                    Address[] in = m.getFrom();
-                            //                    for (Address address : in) {
-                            //                        System.out.println("FROM:" + address.toString());
-                            //                    }
-                            Arrays.stream(m.getFrom()).forEach(System.out::println);
-                            System.out.println("subject: " + m.getSubject()
-                                    + "\nsend date: " + m.getSentDate()
-                                    + "\ncontent: "  +  (String)content);
+                            System.out.println("Simple Handle");
+
+                            Address[] froms = m.getFrom();
+                            String email = froms == null ? null : ((InternetAddress) froms[0]).getAddress();
+                            System.out.println(email);
+                            System.out.println(m.getSubject());
+                            System.out.println(content);
+
+                            //TODO handle
+
+
 
                         } else if (content instanceof Multipart) {
+                            System.out.println("Multipart Handle");
+                            Address[] froms = m.getFrom();
+                            String email = froms == null ? null : ((InternetAddress) froms[0]).getAddress();
+                            System.out.println(email);
+                            System.out.println(m.getSubject());
+                            System.out.println(((Multipart) content).getBodyPart(0).getContent());
 
-                            ArrayList <BodyPart> bps = new ArrayList<>();
-                            for (int i = 0; i < ((Multipart) content).getCount(); i++) {
+                            fileStreams.clear();
+                            for (int i = 1; i < ((Multipart) content).getCount(); i++) {
                                 fileStreams.put(((Multipart) content).getBodyPart(i).getFileName(),
                                         ((Multipart) content).getBodyPart(i).getInputStream());
                             }
 
+
+                            zipCurrentAttachments(m, email);
+
+                            //TODO handle
 
                         }
 
@@ -71,60 +83,60 @@ public class EmailRecieveService {
 
                 }
 
-
-
             } catch (MessagingException e) {
                 e.printStackTrace();
             }
+
             try {
-                m.setFlag(Flags.Flag.SEEN, true);
+                m.setFlag(Flags.Flag.DELETED, true);
             } catch (MessagingException e) {
                 e.printStackTrace();
             }
-        });
-
-
-        System.out.println("FileStream -> " + fileStreams.size());
-
-        String zipFileName = patchfolder + "out.zip";
-        FileOutputStream fos = new FileOutputStream(zipFileName);
-        ZipOutputStream zos = new ZipOutputStream(fos);
-
-
-        fileStreams.entrySet().stream().filter(e -> e.getKey()!= null).forEach(e -> {
-            try {
-                zos.putNextEntry(new ZipEntry(e.getKey()));
-                byte[] bytes = e.getValue().readAllBytes();
-                zos.write(bytes, 0, bytes.length);
-                zos.closeEntry();
-
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
 
         });
-//        fos.close();
-        zos.close();
 
-
-//        inbox.close();
-//        store.close();
-//        System.exit(0);
 
     }
 
 
 
-//    private void writeFileOnDisk(BodyPart bp) throws IOException, MessagingException {
-//
-//        System.out.println("size " + bp.getSize());
-//        FileOutputStream fos = new FileOutputStream("/home/g/Downloads/files/" + bp.getFileName());
-//        DataOutputStream outStream = new DataOutputStream(new BufferedOutputStream(fos));
-//        outStream.write(bp.getInputStream().readAllBytes());
-//        outStream.close();
-//
-//        System.out.println("write: " + bp.getFileName());
-//
-//    }
+
+
+
+    private void zipCurrentAttachments(Message m, String email) throws IOException, MessagingException {
+
+            String zipFileName = patchfolder + email + m.getSentDate();
+            FileOutputStream fos = new FileOutputStream(zipFileName);
+            ZipOutputStream zos = new ZipOutputStream(fos);
+
+
+            fileStreams.entrySet().stream().filter(e -> e.getKey()!= null).forEach(e -> {
+                try {
+                    zos.putNextEntry(new ZipEntry(e.getKey()));
+                    byte[] bytes = e.getValue().readAllBytes();
+                    zos.write(bytes, 0, bytes.length);
+                    zos.closeEntry();
+
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+
+            });
+
+            zos.close();
+            fos.close();
+
+
+
+        }
+
+
+
+
 
 }
+
+
+
+
+
